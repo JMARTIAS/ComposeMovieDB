b696785a1bf553dad24e70cea14c9a000ea3475d
Jose <jmmartinezastroza@gmail.com>
2023-10-09 21:50:14 -0300

    test: add unit test (DB testing)

--- app/src/test/java/com/example/composemoviedb/ExampleUnitTest.kt
+++ app/src/test/java/com/example/composemoviedb/ExampleUnitTest.kt
@@ -1,17 +1,82 @@
 package com.example.composemoviedb
 
+
+import com.example.composemoviedb.data.Movie
+import com.example.composemoviedb.data.MoviesRepository
+import com.example.composemoviedb.data.local.LocalDataSource
+import com.example.composemoviedb.data.remote.RemoteDataSource
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verifyBlocking
 
-import org.junit.Assert.*
+class MoviesRepositoryTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `When DB is empty, server is called`() {
+        val localDataSource = mock<LocalDataSource> {
+            onBlocking { count() } doReturn 0
+        }
+        val remoteDataSource = mock<RemoteDataSource>()
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        runBlocking { repository.requestMovies() }
+
+        verifyBlocking(remoteDataSource) { getMovies() }
+    }
+
+    @Test
+    fun `When DB is empty, movies are saved into DB`() {
+        val expectedMovies = listOf(Movie(1, "Title", "Overview", "poster", false))
+        val localDataSource = mock<LocalDataSource> {
+            onBlocking { count() } doReturn 0
+        }
+        val remoteDataSource = mock<RemoteDataSource> {
+            onBlocking { getMovies() } doReturn expectedMovies
+        }
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        runBlocking { repository.requestMovies() }
+
+        verifyBlocking(localDataSource) { insertAll(expectedMovies) }
+    }
+
+    @Test
+    fun `When DB is no empty, remote data source is not called`() {
+        val localDataSource = mock<LocalDataSource> {
+            onBlocking { count() } doReturn 1
+        }
+        val remoteDataSource = mock<RemoteDataSource>()
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        runBlocking { repository.requestMovies() }
+
+        verifyBlocking(remoteDataSource, times(0)) { getMovies() }
+    }
+
+    @Test
+    fun `When DB is not empty, movies are recovered from DB`() {
+        val localMovies = listOf(Movie(1, "Title", "Overview", "poster", false))
+        val remoteMovies = listOf(Movie(2, "Title2", "Overview2", "poster2", false))
+        val localDataSource = mock<LocalDataSource> {
+            onBlocking { count() } doReturn 1
+            onBlocking { movies } doReturn flowOf(localMovies)
+        }
+        val remoteDataSource = mock<RemoteDataSource> {
+            onBlocking { getMovies() } doReturn remoteMovies
+        }
+        val repository = MoviesRepository(localDataSource, remoteDataSource)
+
+        val result = runBlocking {
+            repository.requestMovies()
+            repository.movies.first()
+        }
+
+        assertEquals(localMovies, result)
     }
 }
